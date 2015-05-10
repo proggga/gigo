36e55356dd00b535f64c460144647d6cfa136bec
Tahia Khan <tahia.khan@gmail.com>
2015-05-10 11:18:57 -0400

    changing directory structure

--- main.go
+++ main.go
@@ -1,44 +1,44 @@
 package main
 
 import (
-  "os"
-  "log"
-  "github.com/codegangsta/cli"
-  "github.com/LyricalSecurity/actions"
+	"github.com/LyricalSecurity/gigo/actions"
+	"github.com/codegangsta/cli"
+	"log"
+	"os"
 )
 
 func main() {
 
-  gopath := os.Getenv("GOPATH")
-  if gopath == "" {
-    log.Fatal("GOPATH environment variable is not set.")
-  }
+	gopath := os.Getenv("GOPATH")
+	if gopath == "" {
+		log.Fatal("GOPATH environment variable is not set.")
+	}
 
-  app := cli.NewApp()
-  app.Name = "gigo"
-  app.Version = "0.3.0"
+	app := cli.NewApp()
+	app.Name = "gigo"
+	app.Version = "0.3.0"
 
-  app.Commands = []cli.Command {
-    {
-      Name: "install",
-      Usage: "Install packages",
+	app.Commands = []cli.Command{
+		{
+			Name:  "install",
+			Usage: "Install packages",
 
-      // command-line sub-flags for instal
-      Flags: []cli.Flag{
-        cli.StringFlag{
-          Name: "r",
-          Usage: "Path to the file listing golang packages to fetch",
-        },
-      },
-      Action: actions.Install,
-    },
-    {
-      Name: "uninstall",
-      Usage: "Uninstall packages",
-      Action: actions.Uninstall,
-    },
-  }
+			// command-line sub-flags for instal
+			Flags: []cli.Flag{
+				cli.StringFlag{
+					Name:  "r",
+					Usage: "Path to the file listing golang packages to fetch",
+				},
+			},
+			Action: actions.Install,
+		},
+		{
+			Name:   "uninstall",
+			Usage:  "Uninstall packages",
+			Action: actions.Uninstall,
+		},
+	}
 
-  app.Run(os.Args)
+	app.Run(os.Args)
 
 }
