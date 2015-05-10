4e6547cf3214040392aab01bebb296bd7e93c08c
Tahia Khan <tahia.khan@gmail.com>
2015-05-10 17:45:31 -0400

    fixing spacing

--- actions/install.go
+++ actions/install.go
@@ -1,65 +1,65 @@
 package actions
 
 import (
-	"github.com/LyricalSecurity/gigo/helpers"
-	"github.com/codegangsta/cli"
-	"io/ioutil"
-	"os"
-	"os/exec"
-	"strings"
+  "github.com/LyricalSecurity/gigo/helpers"
+  "github.com/codegangsta/cli"
+  "io/ioutil"
+  "os"
+  "os/exec"
+  "strings"
 )
 
 func Install(c *cli.Context) {
-	reqs := c.String("r")
+  reqs := c.String("r")
 
-	if reqs == "" {
-		installPackages(c.Args())
-		return
-	}
+  if (reqs == "") {
+    installPackages(c.Args())
+    return
+  }
 
-	_, e := os.Stat(reqs)
-	if os.IsNotExist(e) {
-		println(reqs + " does not exist, exiting.")
-		os.Exit(3)
-	}
+  _, e := os.Stat(reqs)
+  if os.IsNotExist(e) {
+    println(reqs + " does not exist, exiting.")
+    os.Exit(3)
+  }
 
-	file, err := ioutil.ReadFile(reqs)
-	if err != nil {
-		println(err)
-		os.Exit(3)
-	}
+  file, err := ioutil.ReadFile(reqs)
+  if err != nil {
+    println(err)
+    os.Exit(3)
+  }
 
-	contents := strings.Split(string(file), "\n")
-	installPackages(contents)
+  contents := strings.Split(string(file), "\n")
+  installPackages(contents)
 
 }
 
 // this is why GOPATH has to bet set in main.go
 func installFromGoGet(url string) error {
-	c := exec.Command("go", "get", url)
-	err := c.Run()
-	if err != nil {
-		println(err)
-		os.Exit(3)
-	}
+  c := exec.Command("go", "get", url)
+  err := c.Run()
+  if err != nil {
+    println(err)
+    os.Exit(3)
+  }
 
-	return nil
+  return nil
 }
 
 func installPackages(packages []string) {
 
-	for _, elem := range packages {
-		if elem == "" || elem == "\n" {
-			continue
-		}
+  for _, elem := range packages {
+    if (elem == "" || elem == "\n") {
+      continue
+    }
 
-		println("Installing " + elem)
-		if helpers.IsGoGettable(elem) {
-			installFromGoGet(elem)
-		} else {
-			RcsGet(elem)
-		}
+    println("Installing " + elem)
+    if helpers.IsGoGettable(elem) {
+      installFromGoGet(elem)
+    } else {
+      RcsGet(elem)
+    }
 
-	}
+  }
 
 }
